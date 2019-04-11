323ce9f8f9a402f6018a0649e20a5dde0544860b
vitipro <47673221+vitipro@users.noreply.github.com>
2019-04-11 12:36:49 +0200

    Create Cjt_idiomes.hh

---
+++ Cjt_idiomes.hh
@@ -0,0 +1,28 @@
+#include "Idioma.hh"
+#include <vector>
+
+class Cjt_idiomes {
+
+private:
+
+  vector<Idioma> v_idiom;
+  int n_idiom;
+
+public:
+
+  // constructores
+  
+  Cjt_idiomes();           // generica
+  
+  // modificadores
+  
+  void afegir_idioma(const Idioma& I);
+  
+  void modificar_idioma(const Idioma& I)
+  
+  void modificar_idioma_iessim(const Idioma& I, int i);
+  
+  bool existeix_idioma(Idioma I) const;
+  
+  
+  ~Cjt_idiomes();

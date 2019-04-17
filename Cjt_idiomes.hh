585c665058ce20b18d1d921d8bdd2b40b38f946a
vitipro <47673221+vitipro@users.noreply.github.com>
2019-04-17 13:11:11 +0200

    Update Cjt_idiomes.hh

--- Cjt_idiomes.hh
+++ Cjt_idiomes.hh
@@ -11,23 +11,29 @@ public:
 
   // constructores
   
-  Cjt_idiomes();           // generica
+  Cjt_idiomes();          
   
   // consultores
   
   bool existeix_idioma(Idioma I) const;
+  /* Pre: cert */
+  /* Post: retorna true si Idioma hi es al conjunt */
   
   // modificadores
   
   void afegir_idioma(const Idioma& I);
+  /* Pre: Idioma no hi és al conjunt */
+  /* Post: el conjunt conté a Idioma */
   
   void modificar_idioma(const Idioma& I);
-  
-  void modificar_idioma_iessim(const Idioma& I);
+  /* Pre: Idioma hi és al conjunt */
+  /* Post: Idioma ha estat modificat */
   
   // entrada/sortida
   
   void llegir_conjunt();
+  /* Pre: al canal estàndard d'entrada hi ha una sèrie de idiomes */
+  /* Post: els idiomes s'han afegit al conjunt */
   
   ~Cjt_idiomes();
   

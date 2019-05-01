7ad9a2e2541c10d6cd1167b863bc21358204900a
vitipro <47673221+vitipro@users.noreply.github.com>
2019-05-02 01:18:07 +0200

    Create Cjto_idiomas.cc

---
+++ Cjto_idiomas.cc
@@ -0,0 +1,50 @@
+#include "Cjto_idiomas.hh"
+#include "Idioma.hh"
+#include <map>
+#include <iostream>
+using namespace std;
+
+Cjto_idiomas::Cjto_idiomas() {}
+
+Cjto_idiomas::~Cjto_idiomas() {}
+
+bool Cjto_idiomas::existe_idioma(string id) const {
+    map<string, Idioma>::const_iterator it = c.find(id);
+    if (it == c.end()) return false;
+    else return true;
+}
+
+void Cjto_idiomas::anadir_idioma(Idioma& I) {
+    string id = I.consultar_nombre();
+	c.insert(make_pair(id, I));
+}
+
+//void Cjto_idiomas::modificar_idioma(Idioma& I) const {
+//	string id = I.consultar_nombre();
+	//map<string, Idioma>::const_iterator it = c.find(id);
+	//Idioma i = it->second;
+	//it->second = I;
+//}
+
+Idioma Cjto_idiomas::consultar_idioma(string id) const {
+    map<string, Idioma>::const_iterator it = c.find(id);
+	return it->second;
+}
+
+void Cjto_idiomas::leer_conjunto() {
+    int n;
+    Idioma I;
+    cin >> n;
+    for (int i = 0; i < n; ++i) {
+        I.leer_idioma();
+        anadir_idioma(I);
+    }
+}
+
+//void Cjto_idiomas::codifica_idioma(string id, string& texto) {
+
+//}
+
+//void Cjto_idiomas::decodifica_idioma(string id, string& texto) {
+
+//}
